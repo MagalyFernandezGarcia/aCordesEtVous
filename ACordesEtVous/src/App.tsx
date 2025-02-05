4435295eefeebc21e44a3c5c3e2cde97b1f23749
Magaly <fernandezgarciamagaly@gmail.com>
2025-02-05 15:53:10 +0100

    adding react router

--- ACordesEtVous/src/App.tsx
+++ ACordesEtVous/src/App.tsx
@@ -1,8 +1,23 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./Components/homePage/HomePage";
 import Footer from "./Container/Footer/Footer";
 import Header from "./Container/Header/Header";
+import DisplaysPage from "./Components/displaysPage/DisplaysPage";
 
+// function App() {
+//   return (
+//     <>
+
+//     <Header/>
+//     <Routes>
+//       <Route path="/" element={<HomePage />} />
+//       <Route path="/displays" element={<DisplaysPage />} />
+//     </Routes>
+//     <Footer />
+//     </>
+//   );
+// }
 function App() {
   return (
     <section className="app">
@@ -10,7 +25,10 @@ function App() {
         <Header />
       </section>
       <section className="main">
-        <HomePage />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/displays" element={<DisplaysPage />} />
+        </Routes>
       </section>
       <section className="footer">
         <Footer />
