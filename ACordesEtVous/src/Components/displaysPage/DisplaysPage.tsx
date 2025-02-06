993341cb24aa0d658dae726a8181ee6080add319
Magaly <fernandezgarciamagaly@gmail.com>
2025-02-06 10:41:16 +0100

    Amenagements service and page

--- ACordesEtVous/src/Components/displaysPage/DisplaysPage.tsx
+++ ACordesEtVous/src/Components/displaysPage/DisplaysPage.tsx
@@ -1,9 +1,57 @@
-
+import { useEffect, useState } from "react";
+import { fetchDisplayList } from "../../Services/amenagementService";
+import { Amenagement } from "../../Types/amenagements";
+import "./displaysPage.css";
 
 const DisplaysPage = () => {
+  const [displays, setDisplays] = useState<Amenagement[]>([]);
 
-    return(<div>Hello</div>)
-    
-}
+  useEffect(() => {
+    const fetchData = async () => {
+      let ignore = false;
 
-export default DisplaysPage
+      const result = await fetchDisplayList();
+      if (!ignore) {
+        setDisplays(result);
+      }
+
+      return () => {
+        ignore = true;
+      };
+    };
+
+    fetchData();
+  }, []);
+
+  const amenagements = displays.map((display) => {
+    return (
+      <div key={display.id} className="galleryContainer">
+        <h2>{display.nom_de_lambiance}</h2>
+        <div className="gallery">
+          {display.photos.map((photo) => {
+            return (
+              <img
+                key={photo.ID}
+                src={photo.guid}
+                alt="illustration du service"
+                className="photo"
+              />
+            );
+          })}
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <main className="displaysPage">
+      <h1 className="titleDisplays"> Salle</h1>
+      <section className="content">
+        <section className="leftSide">{amenagements}</section>
+        <section className="rightSide"></section>
+      </section>
+    </main>
+  );
+};
+
+export default DisplaysPage;
