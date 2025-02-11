99eac632b418bf99f43e7bf6b953ac50fcf99d41
Magaly <fernandezgarciamagaly@gmail.com>
2025-02-11 10:48:20 +0100

    materialsLocation desktop version

--- ACordesEtVous/src/Components/materialsLocation/MaterialsLocation.tsx
+++ ACordesEtVous/src/Components/materialsLocation/MaterialsLocation.tsx
@@ -1,11 +1,58 @@
+import { useEffect, useState } from "react";
+import { fetchMaterialsList } from "../../Services/getServices";
+import { Material } from "../../Types/materials";
+import "./materialsLocation.css";
 
+const MaterialsLocation = ({
+  onSetCurrentPage,
+}: {
+  onSetCurrentPage: React.Dispatch<React.SetStateAction<string>>;
+}) => {
+  const [materials, setMaterials] = useState<Material[]>([]);
 
-const MaterialsLocation = ({onSetCurrentPage} : {onSetCurrentPage : React.Dispatch<React.SetStateAction<string>>}) => {
+  useEffect(() => {
+    onSetCurrentPage("Accueil");
+    const fetchData = async () => {
+      let ignore = false;
+
+      const result = await fetchMaterialsList();
+
+      if (!ignore) {
+        setMaterials(result);
+      }
+
+      return () => {
+        ignore = true;
+      };
+    };
+
+    fetchData();
+  }, []);
+
+  const displayMaterials = materials.map((material) => {
     return (
-        <div>
-            <h1>MaterialsLocation</h1>
+      <div key={material.id} className="cardLocation">
+        <img
+          src={material.image_de_lobjet.guid}
+          alt="photo du matériel"
+          className="locationImg"
+        />
+        <h2>{material.title.rendered}</h2>
+        <p className="materialDescription">{material.description}</p>
+        <div className="priceContainer">
+          <p>{material.prix}€</p>
+          <p>{material.prix_description}</p>
         </div>
+      </div>
     );
+  });
+
+  return (
+    <main>
+      <h1 className="locationTitle">Location de matériel</h1>
+      <section className="cardsContainer">{displayMaterials}</section>
+    </main>
+  );
 };
 
 export default MaterialsLocation;
